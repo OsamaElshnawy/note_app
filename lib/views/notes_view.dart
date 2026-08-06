a0c15a30614d39dea6a0ff91cfd1304b86acedbe
Osama Elshnawy <songelagyal@gmail.com>
2026-08-06 20:18:52 +0300

    create custom icon in custom app

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search) )],
-      ),
-    );
+    return Scaffold(body: NotesViewBody());
   }
 }
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_app_bar_view.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(children: [CustomAppBarView()]),
+    );
+  }
+}
