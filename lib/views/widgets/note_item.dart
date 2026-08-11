3672acb852ad46ec07cbd8e92bdc2b1558472e3d
Osama Elshnawy <songelagyal@gmail.com>
2026-08-11 22:13:37 +0300

    create edit note view

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,55 +1,67 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0),
-        color: Color(0xffFFCD7A),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 28,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
-              child: const Text(
-                'Note Description you can write here what you want',
-                style: TextStyle(color: Colors.black45, fontSize: 18),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.trash.data,
-                color: Colors.black,
-                size: 28,
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the edit note page
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'aug 6,2026',
-              style: TextStyle(color: Colors.black45, fontSize: 16),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          color: Color(0xffFFCD7A),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter tips',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+                child: const Text(
+                  'Note Description you can write here what you want',
+                  style: TextStyle(color: Colors.black45, fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash.data,
+                  color: Colors.black,
+                  size: 28,
+                ),
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'aug 6,2026',
+                style: TextStyle(color: Colors.black45, fontSize: 16),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
